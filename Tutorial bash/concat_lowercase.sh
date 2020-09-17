b46a4941cd6416280dc40da3ce0c70aa2b9303a2
jajme <33689228+jajme@users.noreply.github.com>
2020-09-17 11:26:44 +0200

    Update concat_lowercase.sh

--- Tutorial bash/concat_lowercase.sh
+++ Tutorial bash/concat_lowercase.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-DIRECTORY=$(pwd)
 cd "$1"
 LOWER_TEXT=$(find -type f -exec python ~/Desktop/Tutoriales/Tutorial\ python/lowercase.py -in {} \;)
 LOWER_TEXT="$LOWER_TEXT \n lowercased"
-echo "$LOWER_TEXT">"$DIRECTORY/lower_txt.txt"
+echo lower_txt.txt"
