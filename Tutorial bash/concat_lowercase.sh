89dc1d7994a8f1daad13fa34567c64002e768879
jajme <33689228+jajme@users.noreply.github.com>
2020-09-17 11:35:16 +0200

    Final review version

--- Tutorial bash/concat_lowercase.sh
+++ Tutorial bash/concat_lowercase.sh
@@ -3,4 +3,4 @@
 cd "$1"
 LOWER_TEXT=$(find -type f -exec python ~/Desktop/Tutoriales/Tutorial\ python/lowercase.py -in {} \;)
 LOWER_TEXT="$LOWER_TEXT \n lowercased"
-echo lower_txt.txt"
+echo "$LOWER_TEXT">"lower_txt.txt"
